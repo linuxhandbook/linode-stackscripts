384781c0146bf236fbedf8c3183098a33d469cd9
Debdut Chakraborty <imdebdut@protonmail.com>
2021-01-20 11:39:46 +0530

    deploy-essentials init.

---
+++ deploy-essentials/main.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# This script changes a total of four things
+# 1. ssh parameters
+# 2. Setting up unattended upgrades
+# 3. Set up a non-root user
+# 4. System upgrade
+# Couple parameters of 1 is optionally manual, 2, 4 are optional as well.
+
+
